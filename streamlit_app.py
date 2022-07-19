47732e42b40ae84319668862ff6e0a496441943e
wkylehoffman <31998872+wkylehoffman@users.noreply.github.com>
2022-07-18 23:36:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.erro import URLError
+from urllib.error import URLError
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
